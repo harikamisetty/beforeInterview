0bfc70061365912b9381916097951f47adb306f4
harikamisetty <43524296+harikamisetty@users.noreply.github.com>
2020-06-08 22:41:27 -0700

    Updated Best Solution

--- strings/BasicCalculater.java
+++ strings/BasicCalculater.java
@@ -3,6 +3,46 @@ package com.hari.dsal.strings;
 import java.util.Stack;
 
 public class BasicCalculater {
+	
+	// Best Solution
+	private int getCalculatedResult(String s) {
+		if(s== null || s.length()==0)
+			return 0;
+
+		Stack<Integer> stack = new Stack<>();
+		char sign ='+';
+		int num =0;
+		int len = s.length();
+
+		for(int i=0; i < s.length(); i++) {
+			if(Character.isDigit(s.charAt(i))) {
+				num = num * 10 + (s.charAt(i) - '0');
+			}
+			if((!Character.isDigit(s.charAt(i)) && ' '!=s.charAt(i) || i == len -1)){
+				if(sign == '-') {
+					stack.push(-num);
+				}
+				if(sign == '+') {
+					stack.push(num);
+				}
+				if(sign == '*') {
+					stack.push(stack.pop() * num);
+				}
+				if(sign == '/') {
+					stack.push(stack.pop() / num);
+				}
+				sign = s.charAt(i);
+				num = 0;
+			}
+		}
+		int re = 0;
+		for(int i : stack) {
+			re+=i;
+		}
+		return re;
+	}
+	
+	// Solution 2
 	public int calculate(String s) {
 		if (s == null || s.length() == 0) {
 			return 0;
