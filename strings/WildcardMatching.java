694b3364aeb099373b31c29e13a3368dce2a4ebc
harikamisetty <43524296+harikamisetty@users.noreply.github.com>
2020-06-10 23:06:57 -0700

    Updated solution

--- strings/WildcardMatching.java
+++ strings/WildcardMatching.java
@@ -1,6 +1,37 @@
 package com.hari.dsal.strings;
 
 public class WildcardMatching {
+	// BEST Solution
+	public boolean isMatch(String s, String p) {
+		int i = 0;
+		int j = 0;
+		int starIndex = -1;
+		int iIndex = -1;
+
+		while (i < s.length()) {
+			if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+				++i;
+				++j;
+			} else if (j < p.length() && p.charAt(j) == '*') {
+				starIndex = j;
+				iIndex = i;
+				j++;
+			} else if (starIndex != -1) {
+				j = starIndex + 1;
+				i = iIndex + 1;
+				iIndex++;
+			} else {
+				return false;
+			}
+		}
+
+		while (j < p.length() && p.charAt(j) == '*') {
+			++j;
+		}
+
+		return j == p.length();
+	}
+	// Solution 2
 	public boolean isMatch(String s, String p) {
 		if (p == null || p.length() == 0) {
 			return s == null || s.length() == 0;
